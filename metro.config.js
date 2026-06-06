a2fb73a1d7b9fc951c8c7fcc45a893619232bc07
Yash Thakkar <yashthakkar0603@gmail.com>
2026-06-06 15:17:24 +0530

    chnage

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,12 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('@react-native/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const expoConfig = getDefaultConfig(__dirname);
+
+const config = mergeConfig(expoConfig, {
+  projectRoot: __dirname,
+});
 
 module.exports = config;
